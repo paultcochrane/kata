522c6824957e479fac4fa46da2869a0b37bea888
Paul Cochrane <paul@liekut.de>
2014-02-13 18:18:38 +0100

    [ruby/fizzbuzz] a first cut at fizzbuzz

---
+++ ruby/fizzbuzz.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+for i in 1..100
+    if i.modulo(3) == 0
+        puts "Fizz"
+    elsif i.modulo(5) == 0
+        puts "Buzz"
+    elsif i.modulo(15) == 0
+        puts "FizzBuzz"
+    else
+        puts i
+    end
+end
+
+# vim: expandtab shiftwidth=4 softtabstop=4
