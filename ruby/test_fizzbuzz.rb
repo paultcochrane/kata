dfa410601096b8895650ef0d217a12199bff34cf
Paul Cochrane <paul@liekut.de>
2014-02-16 14:08:42 +0100

    [ruby/fizzbuzz] adding a test for "buzz"

--- ruby/test_fizzbuzz.rb
+++ ruby/test_fizzbuzz.rb
@@ -11,6 +11,11 @@ class TestFizzBuzz < Test::Unit::TestCase
 	assert_equal("fizz", @fizzbuzz.input(3),
 		     "Should output fizz")
     end
+
+    def test_buzz
+        assert_equal("buzz", @fizzbuzz.input(5),
+                     "Should output buzz")
+    end
 end
 
 # vim: expandtab shiftwidth=4 softtabstop=4
